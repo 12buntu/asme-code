05807dfe573f263d132a47c61d32ad1404fdb87f
12buntu <kylemhollars@gmail.com>
2024-04-04 20:10:55 -0400

    auto restart

--- runcode.py
+++ runcode.py
@@ -1,5 +1,9 @@
 #!/home/blackweldera/venv/bin/python3
 import pygame
+from subprocess import Popen
+from time import sleep
+from os import execl
+import sys
 from controller import Controller
 from hardware import Chassis
 
@@ -15,7 +19,10 @@ def main():
         
         chassis.drive(-gps["y1_axis"], gps["x1_axis"])
         print(round(-gps["y1_axis"],1), round(gps["x1_axis"],1))
-        
+        if gps["b_x"]: 
+            Popen('git pull', shell=True)
+            sleep(2.5)
+            execl(sys.executable, sys.executable, *sys.argv)
         
 if __name__ == "__main__":
     main()
