7cba49654829855d22bee083728488fcd6ff1ada
12buntu <kylemhollars@gmail.com>
2024-04-11 21:20:56 -0400

    reverse

--- runcode.py
+++ runcode.py
@@ -17,8 +17,8 @@ def main():
         gamepad.process_events()
         gps = gamepad.get_controller()
         
-        chassis.drive(-gps["y1_axis"], gps["x1_axis"])
-        print(round(-gps["y1_axis"],1), round(gps["x1_axis"],1))
+        chassis.drive(gps["y1_axis"], -gps["x1_axis"])
+        print(round(gps["y1_axis"],1), -round(gps["x1_axis"],1))
         if gps["b_x"]: 
             Popen('git pull', shell=True)
             sleep(2.5)
