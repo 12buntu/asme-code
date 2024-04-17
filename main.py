47b67f45bacd1d0ce4aaf67ba37188fc16d688ec
12buntu <kylemhollars@gmail.com>
2024-04-16 21:03:13 -0400

    a

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
         gps = gamepad.get_controller()
         ###
         flywheel.send_power((round((gps["r_axis"]) + 1) / 2))
-        print(flywheel.pwm_dev.pulse_width)
+        print(flywheel.pwm_dev.value)
         chassis.drive(gps["y1_axis"], gps["x1_axis"])
         if gps["b_opt"]: 
             Popen('git pull', shell=True)
