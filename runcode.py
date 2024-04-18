6b84143a2c394ceca11b2d44e4cd088ebedbf7b2
12buntu <kylemhollars@gmail.com>
2024-04-17 20:01:09 -0400

    fixed

--- runcode.py
+++ runcode.py
@@ -1,2 +1,2 @@
 from os import popen
-popen('lxterminal -e python3 /home/blackweldera/asme-code/main.py')
+popen('lxterminal -e cd /home/blackweldera/asme-code/ && python3 /home/blackweldera/asme-code/main.py')
