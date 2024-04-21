ccc545c9e5f0e67a242800bfc1a53554b455a08d
12buntu <kylemhollars@gmail.com>
2024-04-21 01:35:33 -0400

    grah

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def main():
         chassis.drive(gps["y1_axis"], gps["x1_axis"])
 
         # Arm Control Scheme
-        spinny.send_power(gps["x_hat"][0])
-        linact.send_power(gps["y_hat"][1])
+        spinny.send_power(gps["x_hat"])
+        linact.send_power(gps["y_hat"])
 
         # Ball Control Scheme
         n20_1.send_power(gps["bump_l"] - gps["bump_r"])
