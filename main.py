1d2d32446b7c8b82c4d73de1fa6242cf03965c80
12buntu <kylemhollars@gmail.com>
2024-04-21 00:45:19 -0400

    switched to hat control

--- main.py
+++ main.py
@@ -46,9 +46,8 @@ def main():
         chassis.drive(gps["y1_axis"], gps["x1_axis"])
 
         # Arm Control Scheme
-        spinny.send_power(-antidrift(gps["x2_axis"]))
-
-        linact.send_power(antidrift(gps["y2_axis"]))
+        spinny.send_power(gps["x_hat"])
+        linact.send_power(antidrift(gps["y_hat"]))
 
         # Ball Control Scheme
         n20_1.send_power(gps["bump_l"] - gps["bump_r"])
